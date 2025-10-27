3562993cc7872ae365936e7e5e9851958bfb728c
Juliana Torres <julianatc21@gmail.com>
2025-10-27 12:49:09 -0500

    Avance Práctica

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        View view = new View();
+
+        while (true) {
+            view.showMenu();
+            int option = view.inputOption(4);
+
+            int[] numeros ={3, 4, 5, 5};
+            switch (option) {
+                case 1:
+                    System.out.println("Método Seleccion");
+                    view.showOrderMenu();
+                    int orden = view.inputOption(2);
+                    boolean ordenOpt = orden == 1;
+                    view.showPasosMenu();
+                    int pasos = view.inputOption(2);
+                    MetodoSeleccion metodoSeleccion = new MetodoSeleccion();
+                    metodoSeleccion.sort(numeros, ordenOpt, pasos == 1);
+
+                    break;
+                case 2:
+                    System.out.println("Método Inserción");
+                    break;
+                case 3:
+                    System.out.println("Método Burbuja");
+                    break;
+                case 4:
+                    System.out.println("Adiós");
+                    System.exit(option);
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
     }
 }
---
+++ src/View.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class View {
+
+    //Variable global privada para el scanner
+    private final Scanner scanner;
+
+    public View(){
+        System.out.println("Metodos Ordenamiento");
+        scanner = new Scanner(System.in);
+    }
+
+    public void showMenu(){
+        System.out.println("\n--Menú métodos--");
+        System.out.println("1. Selection");
+        System.out.println("2. Insertion");
+        System.out.println("3. Bubble");
+        System.out.println("4. Salir");
+        System.err.print("Seleccione la opcion: ");
+    }
+
+    public int inputOption(int max ){
+        int option = -1;
+        while (true) {
+            if (scanner.hasNextInt()) {
+                //si ingreso un numero
+                option = scanner.nextInt();
+                if (option >= 1 && option <= max) 
+                    break;
+            } else {
+                scanner.next();
+            }
+            System.out.print("Opción inválida, ingrese otra vez: ");
+        }
+        return option;
+    }
+
+    public void showOrderMenu(){
+        System.out.println("Seleccione el orden");
+        System.out.println("1. Ascendente");
+        System.out.println("2. Descendente");
+        System.err.print("Seleccione la opcion: ");
+    }
+
+    public void showPasosMenu(){
+        System.out.println("-Quiere ver los pasos-");
+        System.out.println("1. Si");
+        System.out.println("2. No");
+        System.err.print("Seleccione la opcion: ");
+    }
+}
